ec1d6a7b372257956fa71a7c8641f82c56d9311a
Josekariz <joseph.macharia@student.moringaschool.com>
2022-11-08 08:59:17 +0300

    initial commit

---
+++ src/components/App.js
@@ -0,0 +1,14 @@
+import "../App.css";
+function App() {
+  return (
+    
+      <div className="App">
+      <div className="text">H</div>
+      <div className="text">A</div>
+      <div className="text">O</div>
+
+    </div>
+  );
+}
+
+export default App;

b0d9bbb53dae0ed981f5c10cb909b8d44cd918cb
moringa <oscarokumu47@gmail.com>
2022-11-10 21:42:36 +0300

    done with contact component

--- src/Pages/Contact/Contact.js
+++ src/Pages/Contact/Contact.js
@@ -1,67 +1,116 @@
-import React from 'react';
-import axios from 'axios';
-import './Contact.css'
+import React from "react";
+import axios from "axios";
+import Navbar from "../Navbar/Navbar";
+import "./Contact.css";
 class App extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      name: '',
-      email: '',
-      message: ''
-    }
+      name: "",
+      email: "",
+      message: "",
+    };
   }
-  handleSubmit(e){
+  handleSubmit(e) {
     e.preventDefault();
     axios({
       method: "POST",
-      url:"",
-      data:  this.state
-    }).then((response)=>{
-      if (response.data.status === 'success') {
+      url: "",
+      data: this.state,
+    }).then((response) => {
+      if (response.data.status === "success") {
         alert("Message Sent.");
-        this.resetForm()
-      } else if (response.data.status === 'fail') {
-        alert("Message failed to send.")
+        this.resetForm();
+      } else if (response.data.status === "fail") {
+        alert("Message failed to send.");
       }
-    })
+    });
   }
-  resetForm(){
-    this.setState({name: "", email: "", message: ""})
+  resetForm() {
+    this.setState({ name: "", email: "", message: "" });
   }
   render() {
-    return(
-      <div className="App">
-        <form id="contact-form" onSubmit={this.handleSubmit.bind(this)} method="POST">
-          <div className="form-group">
-              <label htmlFor="name">Name</label>
-              <input type="text" className="form-control" id="name" value={this.state.name} onChange={this.onNameChange.bind(this)} />
+    return (
+      <>
+        <div className="row">
+          <div className="dashboard-nav mh-100 col-2">
+            <Navbar />
           </div>
-          <div className="form-group">
-              <label htmlFor="exampleInputEmail1">Email address</label>
-              <input type="email" className="form-control" id="email" aria-describedby="emailHelp" value={this.state.email} onChange={this.onEmailChange.bind(this)} />
           </div>
-          <div className="form-group">
-              <label htmlFor="message">Message</label>
-              <textarea className="form-control" rows="5" id="message" value={this.state.message} onChange={this.onMessageChange.bind(this)} />
+          <div className="App col-6" >
+            <div
+              style={{
+                backgroundColor: "#eaf9ff",
+                maxHeight: "100%",
+                overflowY: "scroll",
+              }}
+              className="dashboard-main mh-100 col-10"
+            >
+              <form
+                id="contact-form"
+                className="contact-form"
+                onSubmit={this.handleSubmit.bind(this)}
+                method="POST"
+              >
+                <div className="form-group">
+                  <label htmlFor="name">Name</label>
+                  <input
+                    type="text"
+                    className="form-control"
+                    placeholder="Enter your name"
+                    id="name"
+                    value={this.state.name}
+                    onChange={this.onNameChange.bind(this)}
+                  />
+                </div>
+                <div className="form-group">
+                  <label htmlFor="exampleInputEmail1">Email address</label>
+                  <input
+                    type="email"
+                    className="form-control"
+                    placeholder="Enter your email"
+                    id="email"
+                    aria-describedby="emailHelp"
+                    value={this.state.email}
+                    onChange={this.onEmailChange.bind(this)}
+                  />
+                </div>
+                <div className="form-group">
+                  <label htmlFor="message">Message</label>
+                  <textarea
+                    className="form-control"
+                    rows="5"
+                    id="message"
+                    placeholder="Leave us your message"
+                    value={this.state.message}
+                    onChange={this.onMessageChange.bind(this)}
+                  />
+                </div>
+                <div className="submit-button">
+                  {" "}
+                  <button type="submit" className="btn btn-primary">
+                    Submit
+                  </button>
+                </div>
+              </form>
+              <div className="Contacts">
+                <h3>Contact us through:</h3>
+                <h3>Email : oscarokumu47@gmail.com</h3>
+              </div>
+            </div>
           </div>
-         <div className='submit-button'> <button type="submit" className="btn btn-primary">Submit</button> 
-         </div>
-        </form>
-        <div className="Contacts">
-            <h3>Contact us through:</h3>
-            <h3>Email : oscarokumu47@gmail.com</h3>
-        </div>
-      </div>
+        
+      </>
     );
   }
   onNameChange(event) {
-	  this.setState({name: event.target.value})
+    this.setState({ name: event.target.value });
   }
   onEmailChange(event) {
-	  this.setState({email: event.target.value})
+    this.setState({ email: event.target.value });
   }
   onMessageChange(event) {
-	  this.setState({message: event.target.value})
+    this.setState({ message: event.target.value });
   }
 }
-export default App
+export default App;
